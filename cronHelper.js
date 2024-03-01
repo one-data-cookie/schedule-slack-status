43582d82b2bec5472b4234d0e7d4f7391f45dae7
one-data-cookie <kolacek.m@gmail.com>
2024-03-02 00:36:23 +0100

    Fix wrong var name

--- cronHelper.js
+++ cronHelper.js
@@ -58,7 +58,7 @@ function getLatestCronTriggerTime(cronExpressions) {
       latestTrigger = lastTrigger;
     }
   });
-  console.log('Latest CRON trigger time: ' + lastTrigger.toString());
+  console.log('Latest CRON trigger time: ' + latestTrigger.toString());
 
   return latestTrigger;
 }
