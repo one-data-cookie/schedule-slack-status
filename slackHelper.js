003a382844ba99af5d0538f9ab60e6f9a6a33112
one-data-cookie <kolacek.m@gmail.com>
2024-03-08 22:56:13 +0100

    Add charset to contentType of POSTs

--- slackHelper.js
+++ slackHelper.js
@@ -40,7 +40,7 @@ function checkSlackStatusIsEmpty() {
 function setDoNotDisturb(numMinutes, token) {
   var options = {
     "method": "post",
-    "contentType": "application/x-www-form-urlencoded",
+    "contentType": "application/x-www-form-urlencoded; charset=utf-8",
     "headers": {
       "Authorization": "Bearer " + token
     },
@@ -72,7 +72,7 @@ function setSlackStatus(statusEmoji, statusText, expirationMin, dnd) {
 
   var options = {
     "method": "post",
-    "contentType": "application/json",
+    "contentType": "application/json; charset=utf-8",
     "headers": {
       "Authorization": "Bearer " + token
     },
